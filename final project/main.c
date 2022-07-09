12676d57574de281d41e922d1356be1f35e67703
Hashim Sobhi <99601907+Hashim-Sobhi@users.noreply.github.com>
2022-07-09 21:43:12 +0200

    Add files via upload

---
+++ final project/main.c
@@ -0,0 +1,38 @@
+/***********************************************************************************/
+/* Author    : Radwa Ayman  & Hashim Sobhy                                        */
+/* Title     : C final project  (Clinic Management System)                       */
+/* Sub_Title : main file                                                        */
+/*******************************************************************************/
+include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "STD_TYPES.h"
+#include "CLINC.h"
+
+
+void main(void)
+{
+    system("cls");
+
+    //the Appointments//
+    //--------------------------------------
+    strcpy(arr[0].appo,"from 2:pm to 2:30am");
+    arr[0].id_1=0;
+    strcpy(arr[1].appo,"from 2:30pm to 3am");
+    arr[1].id_1=0;
+    strcpy(arr[2].appo,"from 3pm to 3:30pm");
+    arr[2].id_1=0;
+    strcpy(arr[3].appo,"from 3:30pm to 4pm");
+    arr[3].id_1=0;
+    strcpy(arr[4].appo,"from 4pm to 4:30pm");
+    arr[4].id_1=0;
+    //--------------------------------------
+
+
+    main_menu();
+
+
+
+
+
+}
